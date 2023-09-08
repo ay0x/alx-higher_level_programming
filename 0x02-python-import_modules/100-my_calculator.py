5fe3dcf182815bd34090a09e272fbf6bb4a6e8fc
Ayo Idoko <ayo.idoko007@gmail.com>
2023-09-08 23:44:45 +0200

    Updated

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv, exit
+    from calculator_1 import add, sub, mul, div
+    count = len(argv) - 1
+    if count != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif argv[2] not in ('+', '-', '*', '/'):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+
+        if argv[2] == '+':
+            print("{} {} {} = {}".format(a, argv[2], b, add(a, b)))
+
+        if argv[2] == '-':
+            print("{} {} {} = {}".format(a, argv[2], b, sub(a, b)))
+
+        if argv[2] == '*':
+            print("{} {} {} = {}".format(a, argv[2], b, mul(a, b)))
+
+        if argv[2] == '/':
+            print("{} {} {} = {}".format(a, argv[2], b, mul(a, b)))
