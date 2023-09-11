a87a49441cd3921d42966fa3ee3b74075ba1515f
Ayo Idoko <ayo.idoko007@gmail.com>
2023-09-11 21:52:10 +0200

    updated

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 a = 89
 b = 10
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+temp1 = a
+temp2 = b
+del a
+del b
+a = temp2
+b = temp1
+
 print("a={:d} - b={:d}".format(a, b))
