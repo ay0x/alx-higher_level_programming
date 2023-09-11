bbe3da6c5c470b22deaa02c558ec6660395c1b07
Ayo Idoko <ayo.idoko007@gmail.com>
2023-09-11 17:47:06 +0200

    Updated

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2:
-        return (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
-    elif len(tuple_b) < 2:
-        return (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
-    elif len(tuple_a) > 2:
-        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    elif len(tuple_b) > 2:
-        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    else:
-        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    padded_tuple_a = (tuple_a[0] if len(tuple_a) > 0 else 0,
+                      tuple_a[1] if len(tuple_a) > 1 else 0)
+    padded_tuple_b = (tuple_b[0] if len(tuple_b) > 0 else 0,
+                      tuple_b[1] if len(tuple_b) > 1 else 0)
+    result_tuple = (padded_tuple_a[0] + padded_tuple_b[0],
+                    padded_tuple_a[1] + padded_tuple_b[1])
+    return result_tuple
