d5dced51d178898e38bb44971a64e99f38696c99
Ayo Idoko <ayo.idoko007@gmail.com>
2023-09-11 18:51:59 +0200

    Updated

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    return max(my_list)
+    if my_list:
+        max = my_list[0]
+        for i in my_list:
+            if my_list[i] > max:
+                return my_list[i]
+    else:
+        return None
