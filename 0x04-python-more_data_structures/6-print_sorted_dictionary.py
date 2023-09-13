d42ad9a67653f37e8fd48ebb53182e8099d96e1a
Ayo Idoko <ayo.idoko007@gmail.com>
2023-09-14 01:33:50 +0200

    Updated

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     sorted_items = sorted(a_dictionary.items())
     for key, value in sorted_items:
-        print(key, ':', value)
+        print("{}: {}".format(key, value))
