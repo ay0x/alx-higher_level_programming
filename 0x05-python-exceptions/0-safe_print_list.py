8888fd5ee456048f18e8415ff701c5a6e085a14e
Ayo Idoko <ayo.idoko007@gmail.com>
2023-09-25 18:00:45 +0200

    Updated

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for i in my_list:
+            if count >= x:
+                break
+            print(i, end='')
+            count += 1
+        print()
+        return count
+    except IndexError:
+        pass
