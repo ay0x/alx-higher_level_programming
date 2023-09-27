63639259ad994376a3e6679659ce17e21e6056f0
Ayo Idoko <ayo.idoko007@gmail.com>
2023-09-28 01:46:56 +0200

    Updated

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
+"""Define a class Square."""
+
 
 class Square:
-    """
-    This is an an empty class Square that defines a square
-    """
+    """Represent a square."""
     pass
