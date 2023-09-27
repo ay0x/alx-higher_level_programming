89445d87544f6ac03b4c83fbc3ccf4169fe6b7a6
Ayo Idoko <ayo.idoko007@gmail.com>
2023-09-28 01:38:53 +0200

    Updated

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""
+This is a Class that defines a sqare
+"""
+
+
+class Square:
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+This is a README file that most likely won't be updated later in the project
