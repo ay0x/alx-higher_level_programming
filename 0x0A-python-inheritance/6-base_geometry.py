99d4871cdae0c4b2996c6cbbd341d71275f970f8
Ayo Idoko <ayo.idoko007@gmail.com>
2023-10-10 02:24:24 +0200

    Updated

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Defines an empty class"""
+
+
+class BaseGeometry:
+    """Represents a geometry"""
+    def area(self):
+        """Raise Exception"""
+        raise Exception("area() is not implemented")
