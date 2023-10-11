76cdbbd0bd0bbe338b993ad54eea42db425417b8
Ayo Idoko <ayo.idoko007@gmail.com>
2023-10-11 08:15:15 +0200

    Updated

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Function that prints content of a file"""
+
+
+def read_file(filename=""):
+    """
+    Reads data from a file source
+
+    Args:
+        filename (str, optional): The file source.
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data)
