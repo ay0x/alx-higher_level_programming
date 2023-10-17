e59eef58f4bf7317c7f37c8ce8a9e8f8e2d17466
Ayo Idoko <ayo.idoko007@gmail.com>
2023-10-17 14:24:32 +0200

    Updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """Class definition"""
+import json
 
 
 class Base:
     """Definition of Base class"""
 
     __nb_objects = 0
+
     def __init__(self, id=None):
-        
+
         """initializing instances
 
         Args:
@@ -18,3 +20,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON representation of a string object."""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
