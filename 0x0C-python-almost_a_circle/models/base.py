48f440ee0ffd5889301940788a7e779b78189db5
Ayo Idoko <ayo.idoko007@gmail.com>
2023-10-18 00:46:51 +0200

    Updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -43,6 +43,6 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """Returns the list of the JSON string representation"""
-        if json_string is None:
+        if json_string is None or json_string == "":
             return "[]"
         return json.loads(json_string)
