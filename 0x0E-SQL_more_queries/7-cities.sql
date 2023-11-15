8456913e7630d23efe76ebd6d23515b9db6b8360
Ayo Idoko <ayo.idoko007@gmail.com>
2023-11-16 01:27:18 +0200

    Updated

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,14 +1,14 @@
 -- Create a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
---Switch to databse
+-- Switch to databse
 USE hbtn_0d_usa;
 
 -- Create a table
 CREATE TABLE IF NOT EXISTS cities(
     id INT AUTO_INCREMENT NOT NULL,
     state_id INT NOT NULL,
-    name VARCHAR(256),
+    name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (state_id) REFERENCES states(id)
 );
