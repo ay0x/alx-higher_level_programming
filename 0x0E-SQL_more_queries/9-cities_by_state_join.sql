a4b42873ed43e238ac9acaf437be612811accf9a
Ayo Idoko <ayo.idoko007@gmail.com>
2023-11-16 13:45:26 +0200

    Updated

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 -- Join Cities and States
 SELECT cities.id, cities.name, states.name
 FROM cities
-LEFT JOIN states on states.id = cities.states_id
-ORDER BY cities.id ASC;
+LEFT JOIN states
+ON states.id = cities.state_id
+ORDER BY cities.id;
