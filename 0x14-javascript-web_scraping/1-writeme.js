634453d8a8a40f8dc2f844bf9c813894958c6f37
ay0x <85720179+ay0x@users.noreply.github.com>
2024-01-31 00:41:21 +0200

    Update

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,16 @@
+#!/usr/local/bin/node
+
+const fs = require('fs');
+const filePath = process.argv[2];
+const text = process.argv[3];
+const encoding = 'utf-8';
+
+if (!filePath) {
+  console.error('Provide a file path as the first argument');
+}
+
+fs.writeFile(filePath, text, encoding, (err, data) => {
+  if (err) {
+    console.error(err);
+  }
+});
